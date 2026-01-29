9e75d03d9171df8af800f7b42915a9110a14e3a0
sui-feng-cb <2518179942@qq.com>
2026-01-29 11:57:25 +0800

    Upd: dev_tools

--- dev_tools/war_archives_update.py
+++ dev_tools/war_archives_update.py
@@ -265,7 +265,7 @@ if __name__ == '__main__':
     # input strings in YYYYMMDD format for event time, or input event name
     # use 'recent' for the latest CN enent, use a list to input multiple values,
     # such as '20220428', '虹彩的终幕曲', 'recent', ['雄鹰的叙事歌' , '20220428', 'recent']
-    WarArchivesUpdater.event = '飓风与青春之泉'
+    WarArchivesUpdater.event = '湮烬尘墟'
 
     updater = WarArchivesUpdater()
 
