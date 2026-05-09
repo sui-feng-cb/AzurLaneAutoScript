57d5f308aa889f152bc7e3fa8794c9cfd9f84246
sui-feng-cb <2518179942@qq.com>
2026-05-10 04:32:59 +0800

    Add: buy dorm food when oil overflow during commission receive

--- module/commission/commission.py
+++ module/commission/commission.py
@@ -11,6 +11,7 @@ from module.commission.preset import DICT_FILTER_PRESET, SHORTEST_FILTER
 from module.commission.project import COMMISSION_FILTER, Commission
 from module.config.config_generated import GeneratedConfig
 from module.config.utils import get_server_last_update, get_server_next_update, nearest_future
+from module.dorm.dorm import RewardDorm
 from module.exception import GameStuckError
 from module.handler.info_handler import InfoHandler
 from module.logger import logger
@@ -505,6 +506,7 @@ class RewardCommission(UI, InfoHandler):
         logger.hr('Reward receive')
 
         reward = False
+        food = False
         click_timer = Timer(1)
         with self.stat.new(
                 'commission', method=self.config.DropRecord_CommissionRecord
@@ -555,6 +557,16 @@ class RewardCommission(UI, InfoHandler):
                     # no need to reset click_timer, just instant click REWARD_1
                     # click_timer.reset()
                     continue
+                # handle oil maxed
+                # run once to prevent accidental oil consumption
+                if self.config.SERVER in ['cn']:
+                    if not food and self.appear(OIL_MAXED, offset=(20, 20), interval=3):
+                        logger.info("Oil maxed, buy food to consume oil")
+                        RewardDorm(self.config, self.device).dorm_run(
+                            feed=False, collect=False, buy_furniture=False, buy_food=10)
+                        self.ui_ensure(page_reward)
+                        food = True
+                        continue
                 # Check GET_SHIP at last to handle random white background at page_main
                 for button in [GET_SHIP]:
                     if click_timer.reached() and self.appear(button, interval=1):
